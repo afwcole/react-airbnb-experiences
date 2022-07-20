fb77b486dfed164a40f07cb826eac76abfe72cc8
Adrian Cole <adriancole@Adrians-MacBook-Pro.local>
2022-07-20 04:44:40 +0000

    clone project of the Airbnb Experiences Webpage

---
+++ src/components/Card.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+export default function Card(props) {
+    console.log(props)
+    let badgeText
+    if (props.openSpots === 0){
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
+    return (
+        <div className="card">
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img className="card--image" src={`../images/${props.coverImg}`}/>
+            <div className="card--stats">
+                <img className="card--star" src="../images/star.png"/>
+                <span>{props.stats.rating}</span>
+                <span className="grey">({props.stats.reviewCount}) *</span>
+                <span className="grey">{props.location}</span>
+            </div>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price"><span className="bold">{`From $${props.price}`}</span> / person</p>
+        </div>
+    )
+}
